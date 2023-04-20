d01bc8bd55a382541f8e44bce291c82013596c85
Francesco Granella <francesco.granella@phd.unibocconi.it>
2023-04-20 16:29:57 +0200

    Update predict.py

--- spandimentotool/predict.py
+++ spandimentotool/predict.py
@@ -4,7 +4,9 @@ import pathlib
 import os, sys
 
 import matplotlib.dates as mdates
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
+import pandas as pd
 from pycaret.regression import *
 from scipy.stats import spearmanr
 
@@ -45,82 +47,41 @@ popgrid = get_popgrid()
 processed_weather_forecasts = pd.merge(popgrid.drop(columns='pop'), processed_weather_forecasts.reset_index(),
                                        on=['lon', 'lat'], how='inner')
 
-if code:
-    codcom = code
-    mod = load_model(context.projectpath() / f'models/model_{codcom}')
-    load_config(context.projectpath() / f'models/config_{codcom}')
-    w = processed_weather_forecasts.copy()
-    g = w[(w.codcom == codcom) & (w.date.isin(w.date.drop_duplicates().nlargest(10)))]
-    g = g.sort_values(by='date')
-    g['pred'] =  predict_model(mod, g.drop(columns='date')).rename(columns={'Label': 'pred'})[['pred']]
-    g = pd.merge(g, popgrid, on=['lon', 'lat', 'codcom'], how='inner')
-    prec = g.groupby('date').tp_l0.sum().reset_index()
-    g = g.groupby(['date']).apply(lambda x: (x['pred'] * x['pop'] / x['pop'].sum()).sum()).reset_index().rename(
-        columns={0: 'pred'})
-    g = pd.merge(g, prec)
+mod = load_model(context.projectpath() / f'models/model_{code}_nospand')
+load_config(context.projectpath() / f'models/config_{code}_nospand')
+w = processed_weather_forecasts.copy()
+g = w[(w.codcom == code) & (w.date.isin(w.date.drop_duplicates().nlargest(10)))]
+g = g.sort_values(by='date')
+g['pred'] = predict_model(mod, g.drop(columns='date')).rename(columns={'Label': 'pred'})[['pred']]
+g = pd.merge(g, popgrid, on=['lon', 'lat', 'codcom'], how='inner')
+prec = g.groupby('date').tp_l0.sum().reset_index()
+g = g.groupby(['date']).apply(lambda x: (x['pred'] * x['pop'] / x['pop'].sum()).sum()).reset_index().rename(
+    columns={0: 'pred'})
+g = pd.merge(g, prec)
 
-    from tabulate import tabulate
-    res = g.rename(columns={'pred':'predicted PM2.5', 'tp_l0': 'total precipitation'}).round(2)
-    res.to_csv(output_path, index=False)
-    print(res)
+res = g.rename(columns={'pred': 'predicted PM2.5', 'tp_l0': 'total precipitation'}).round(2)
+res.to_csv(output_path, index=False)
+print(res)
 
-    fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(12, 8), constrained_layout=False, dpi=80)
-    ax1.plot(g.date, g.pred, zorder=1, color='black')
-    ax1.axhline(20, color='red', zorder=0)
-    ax1.axhline(15, color='salmon', zorder=0)
-    ax1.set_ylim(0, ax1.get_ylim()[1])
-    ax1.set_xticklabels(())
-    ax1.spines[['top', 'right']].set_visible(False)
-    ax1.grid(False)
-    ax1.set_ylabel('PM$_{2.5}$, $\mu g/m^3$', fontsize=9)
-    ax2.bar(g.date, g.tp_l0, color='deepskyblue', zorder=0)
-    ax2.set_ylabel('Precipitation, mm', color='deepskyblue', fontsize=9)
-    ax2.spines[['top', 'right']].set_visible(False)
-    ax2.grid(False)
-    ax2.set_xticklabels(g.date, rotation=45, ha='right')
-    myFmt = mdates.DateFormatter('%b%d')
-    ax2.xaxis.set_major_formatter(myFmt)
-    oos = predict_model(mod)
-    title = get_name_comune(codcom) + '\nRank-correlation: ' + str(np.round(spearmanr(oos.y, oos.Label)[0], 2))
-    plt.suptitle(title)
-    plt.show()
-
-else:
-
-    # %%
-    from tqdm import tqdm
-
-    l = []
-    for codcom, g in tqdm(processed_weather_forecasts.groupby('codcom')):
-        try:
-            mod = load_model(context.projectpath() / f'models/model_{codcom}')
-            load_config(context.projectpath() / f'models/config_{codcom}')
-        except:
-            continue
-        g = g.sort_values(by='date')
-        _g = g.iloc[-10:]
-        predictions = predict_model(mod, _g.drop(columns='date')).rename(columns={'Label': 'pred'})[['pred']]
-        predictions['codcom'] = codcom
-        predictions['date'] = _g.date
-        l.append(predictions)
-
-    df = pd.concat(l)
-
-    borders_comuni = get_borders_comuni()
-    gdf = borders_comuni.merge(df, on=['codcom'], how='right')
-    lomb = borders_comuni.dissolve()
-
-    nrows = int(np.ceil(gdf.date.nunique() ** .5))
-    ncols = int(np.floor(gdf.date.nunique() ** .5))
-
-    fig, axs = plt.subplots(nrows, ncols, figsize=(15, 15), sharex=True, sharey=True)
-    for i, (date, g) in enumerate(gdf.groupby('date')):
-        ax = axs.flatten()[i]
-        ax.spines[['top', 'bottom', 'left', 'right']].set_visible(False)
-        lomb.boundary.plot(ax=ax)
-        g.plot(column='pred', ax=ax, legend=True)
-        ax.set_title(date)
-    for ax in axs.flatten()[nrows * ncols - (nrows * ncols - gdf.date.nunique()):]:
-        ax.set_visible(False)
-    plt.tight_layout()
-    plt.show()
+fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(12, 8), constrained_layout=False, dpi=80)
+ax1.plot(g.date, g.pred, zorder=1, color='black')
+ax1.axhline(25, color='red', zorder=0)
+ax1.axhline(15, color='salmon', zorder=0)
+ax1.set_ylim(0, ax1.get_ylim()[1])
+ax1.set_xticklabels(())
+ax1.spines[['top', 'right']].set_visible(False)
+ax1.grid(False)
+ax1.set_ylabel('PM$_{2.5}$, $\mu g/m^3$', fontsize=9)
+custom_lines = [Line2D([0], [0], color='red', lw=4, label='Proposed EU standards'), Line2D([0], [0], color='salmon', lw=4, label='Suggested WHO standards')]
+ax1.legend(handles=custom_lines, loc='center', frameon=False)
+ax2.bar(g.date, g.tp_l0, color='deepskyblue', zorder=0)
+ax2.set_ylabel('Precipitation, mm', color='deepskyblue', fontsize=9)
+ax2.spines[['top', 'right']].set_visible(False)
+ax2.grid(False)
+ax2.set_xticklabels(g.date, rotation=45, ha='right')
+myFmt = mdates.DateFormatter('%b%d')
+ax2.xaxis.set_major_formatter(myFmt)
+oos = predict_model(mod)
+title = get_name_comune(code) + '\nRank-correlation: ' + str(np.round(spearmanr(oos.y, oos.Label)[0], 2))
+plt.suptitle(title)
+plt.show()

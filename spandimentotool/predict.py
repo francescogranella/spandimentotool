4e044f68c7197d3f7c343e12efc171f0795332e9
Francesco Granella <francesco.granella@phd.unibocconi.it>
2023-05-11 16:26:53 +0200

    predict for whole region

--- spandimentotool/predict.py
+++ spandimentotool/predict.py
@@ -4,6 +4,7 @@ import pathlib
 import os, sys
 
 import matplotlib.dates as mdates
+import numpy as np
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -18,9 +19,11 @@ from spandimentotool.utils import preprocess_weather, get_borders_comuni, get_po
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="path to input file", default='', required=False)
 parser.add_argument("-o", "--output", help="path to output file", default='output.csv', required=False)
-parser.add_argument("-c", "--code", help="Code comune", required=False, default=15146)
+parser.add_argument("-c", "--code", help="Code comune", required=False, default=None)
 args = parser.parse_args()
-code = int(vars(args)['code'])
+code = vars(args)['code']
+if code:
+    code = int(code)
 output_path = vars(args)['output']
 
 if vars(args)['input'] == '':
@@ -29,7 +32,7 @@ if vars(args)['input'] == '':
         example_answer = input('No input file or example file selected. Do you want to run an example? y/n\n')
         if example_answer == 'y':
             import os
-            input_path = context.projectpath() / "data/example.parquet"
+            input_path = context.projectpath() / "data/example.nc"
         elif example_answer == 'n':
             print('Bye!')
             sys.exit()
@@ -47,56 +50,101 @@ popgrid = get_popgrid()
 processed_weather_forecasts = pd.merge(popgrid.drop(columns='pop'), processed_weather_forecasts.reset_index(),
                                        on=['lon', 'lat'], how='inner')
 
-mod = load_model(context.projectpath() / f'models/model_{code}_nospand')
-load_config(context.projectpath() / f'models/config_{code}_nospand')
 w = processed_weather_forecasts.copy()
-g = w[(w.codcom == code) & (w.date.isin(w.date.drop_duplicates().nlargest(10)))]
-g = g.sort_values(by='date')
-g['pred'] = predict_model(mod, g.drop(columns='date')).rename(columns={'Label': 'pred'})[['pred']]
-g = pd.merge(g, popgrid, on=['lon', 'lat', 'codcom'], how='inner')
-prec = g.groupby('date').tp_l0.sum().reset_index()
-g = g.groupby(['date']).apply(lambda x: (x['pred'] * x['pop'] / x['pop'].sum()).sum()).reset_index().rename(
-    columns={0: 'pred'})
-g = pd.merge(g, prec)
 
-res = g.rename(columns={'pred': 'predicted PM2.5', 'tp_l0': 'total precipitation'}).round(2)
-res.to_csv(output_path, index=False)
-print(res)
+if code == None:
+    import matplotlib
+    import geopandas as gpd
 
-ranking = g.sort_values(by='date', ascending=False).rolling(2, on='date', min_periods=1, closed='right').mean().sort_values(by='date', ascending=True)
-ranking['prec>0'] = (ranking.tp_l0 > 0).astype(int)
-ranking = ranking.sort_values(by=['prec>0', 'pred'], ascending=[True, True]).reset_index(drop=True).reset_index()
+    matplotlib.rcParams.update(matplotlib.rcParamsDefault)
 
-ranking['index'] += 1
-ranking['date'] = ranking.date.dt.strftime('%b %d')
-# ranking['pred'] = ranking['pred'].astype(float).round(2).astype(str)
-# ranking['tp_l0'] = ranking['tp_l0'].astype(float).round(2).astype(str)
-ranking = ranking[['index', 'date']]
+    w = processed_weather_forecasts.copy()
+    mod = load_model(context.projectpath() / f'models/model_Lombardia_nospand')
+    load_config(context.projectpath() / f'models/config_Lombardia_nospand')
 
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, figsize=(8, 8), constrained_layout=False, dpi=100)
-ax1.plot(g.date, g.pred, zorder=1, color='black')
-ax1.axhline(25, color='red', zorder=0)
-ax1.axhline(15, color='salmon', zorder=0)
-ax1.set_ylim(0, ax1.get_ylim()[1])
-ax1.set_xticklabels(())
-ax1.spines[['top', 'right']].set_visible(False)
-ax1.grid(False)
-ax1.set_ylabel('PM$_{2.5}$, $\mu g/m^3$', fontsize=12)
-custom_lines = [Line2D([0], [0], color='red', lw=4, label='Proposed EU standards'), Line2D([0], [0], color='salmon', lw=4, label='WHO standards')]
-ax1.legend(handles=custom_lines, frameon=False)
-ax2.bar(g.date, g.tp_l0, color='deepskyblue', zorder=0)
-ax2.set_ylabel('Precipitation, mm', color='deepskyblue', fontsize=12)
-ax2.spines[['top', 'right']].set_visible(False)
-ax2.grid(False)
-ax2.set_xticklabels(g.date, rotation=45, ha='right')
-ax3.table(cellText=ranking.values, colLabels=['Rank of best days for spreading', 'Date'], loc='center', cellLoc='center', edges='open')
-ax3.axis('off')
-ax3.axis('tight')
-plt.subplots_adjust(hspace=0.35)
-myFmt = mdates.DateFormatter('%b%d')
-ax2.xaxis.set_major_formatter(myFmt)
-oos = predict_model(mod)
-title = get_name_comune(code) + '\nRank-correlation: ' + str(np.round(spearmanr(oos.y, oos.Label)[0], 2))
-plt.suptitle(title)
-plt.tight_layout()
-plt.show()
+    borders = get_borders_comuni()
+    lomb = borders.dissolve().boundary
+
+    w['pred'] = predict_model(mod, w.drop(columns='date')).rename(columns={'Label': 'pred'})[['pred']]
+    gdf = gpd.GeoDataFrame(w, geometry=gpd.points_from_xy(w.lon, w.lat, crs=4326))
+    gdf['geometry'] = gdf['geometry'].buffer(0.05, cap_style=3)
+    gdf['date'] = gdf.date.dt.strftime('%b %d')
+    gs = gdf.groupby('date')
+
+    npanels = gdf.date.nunique()
+    ncols = int(np.ceil((npanels**0.5)))
+    nrows = int(npanels % ncols)
+
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8,8), sharex=True, sharey=True, dpi=150)
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    cbar = False
+    vmin = gdf.pred.min()
+    vmax = gdf.pred.max()
+    axs = axs.ravel()
+    for i, (ax, (date, g)) in enumerate(list(zip(axs[:npanels], gs))):
+        if i+1 == npanels:
+            cbar = True
+        g.plot(ax=ax, column='pred', cmap='viridis', legend=cbar, vmin=vmin, vmax=vmax,
+            legend_kwds={"label": "Predicted PM$_{2.5}$, $\mu g/m^3$", "orientation": "vertical", "shrink":0.8})
+        lomb.plot(ax=ax, color='black')
+        ax.set_title(date)
+        ax.grid(False)
+        ax.set_axis_off()
+    axs[-1].axis('off')
+    plt.tight_layout()
+    plt.show()
+
+else:
+    w = processed_weather_forecasts.copy()
+    mod = load_model(context.projectpath() / f'models/model_{code}_nospand')
+    load_config(context.projectpath() / f'models/config_{code}_nospand')
+    g = w[(w.codcom == code) & (w.date.isin(w.date.drop_duplicates().nlargest(10)))]
+    g = g.sort_values(by='date')
+    g['pred'] = predict_model(mod, g.drop(columns='date')).rename(columns={'Label': 'pred'})[['pred']]
+    g = pd.merge(g, popgrid, on=['lon', 'lat', 'codcom'], how='inner')
+    prec = g.groupby('date').tp_l0.sum().reset_index()
+    g = g.groupby(['date']).apply(lambda x: (x['pred'] * x['pop'] / x['pop'].sum()).sum()).reset_index().rename(
+        columns={0: 'pred'})
+    g = pd.merge(g, prec)
+
+    res = g.rename(columns={'pred': 'predicted PM2.5', 'tp_l0': 'total precipitation'}).round(2)
+    res.to_csv(output_path, index=False)
+    print(res)
+
+    ranking = g.sort_values(by='date', ascending=False).rolling(2, on='date', min_periods=1, closed='right').mean().sort_values(by='date', ascending=True)
+    ranking['prec>0'] = (ranking.tp_l0 > 0).astype(int)
+    ranking = ranking.sort_values(by=['prec>0', 'pred'], ascending=[True, True]).reset_index(drop=True).reset_index()
+
+    ranking['index'] += 1
+    ranking['date'] = ranking.date.dt.strftime('%b %d')
+    # ranking['pred'] = ranking['pred'].astype(float).round(2).astype(str)
+    # ranking['tp_l0'] = ranking['tp_l0'].astype(float).round(2).astype(str)
+    ranking = ranking[['index', 'date']]
+
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, figsize=(8, 8), constrained_layout=False, dpi=100)
+    ax1.plot(g.date, g.pred, zorder=1, color='black')
+    ax1.axhline(25, color='red', zorder=0)
+    ax1.axhline(15, color='salmon', zorder=0)
+    ax1.set_ylim(0, ax1.get_ylim()[1])
+    ax1.set_xticklabels(())
+    ax1.spines[['top', 'right']].set_visible(False)
+    ax1.grid(False)
+    ax1.set_ylabel('PM$_{2.5}$, $\mu g/m^3$', fontsize=12)
+    custom_lines = [Line2D([0], [0], color='red', lw=4, label='Proposed EU standards'), Line2D([0], [0], color='salmon', lw=4, label='WHO standards')]
+    ax1.legend(handles=custom_lines, frameon=False)
+    ax2.bar(g.date, g.tp_l0*1000, color='deepskyblue', zorder=0)
+    ax2.set_ylabel('Precipitation, mm', color='deepskyblue', fontsize=12)
+    ax2.spines[['top', 'right']].set_visible(False)
+    ax2.grid(False)
+    ax2.set_xticklabels(g.date, rotation=45, ha='right')
+    ax3.table(cellText=ranking.values, colLabels=['Rank of best days for spreading', 'Date'], loc='center', cellLoc='center', edges='open')
+    ax3.axis('off')
+    ax3.axis('tight')
+    plt.subplots_adjust(hspace=0.35)
+    myFmt = mdates.DateFormatter('%b%d')
+    ax2.xaxis.set_major_formatter(myFmt)
+    oos = predict_model(mod)
+    title = get_name_comune(code) + '\nRank-correlation: ' + str(np.round(spearmanr(oos.y, oos.Label)[0], 2))
+    plt.suptitle(title)
+    plt.tight_layout()
+    plt.show()
